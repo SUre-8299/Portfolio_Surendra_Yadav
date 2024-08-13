dfebcf7f4e7eb9b3f087f6034d034777e09c1fa4
“SUre8299” <surendrayadav7668@gmail.com>
2024-08-13 16:02:59 +0530

    Initial commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,26 @@
+setTimeout(function() {
+  $('.about-info-inner').typed({
+    strings: [
+      `<span>Hi, I am Surendra Yadav,</span>
+      a dedicated IT professional from Lucknow, Uttar Pradesh. I specialize in full stack web development, with a strong foundation in HTML, CSS, JavaScript, Bootstrap, React.js, Redux, Node.js, and Express.js. I also have experience with MySQL and MongoDB databases, and tools like Git, XAMPP, and WebSocket’s.
+
+      Currently, I work as a Web Developer at Delhi Apartments Pvt. Ltd., focusing on front-end and back-end development, database management, and API integration. Previously, I was a Full Stack Web Developer at Hanumant Technology Pvt. Ltd., where I developed dynamic and responsive web solutions.
+
+      Before my career in IT, I worked as a Project Engineer at ADJ Engineering Pvt. Ltd. and as a Site Engineer at RSV Infra–Power Pvt. Ltd., gaining valuable project management experience.
+
+      I have worked on various projects, including car servicing applications, e-commerce websites, and wedding planner websites, utilizing my full stack development skills. 
+
+      In my free time, I enjoy listening to music, performing in plays, writing, and gaming.
+
+      <b>Contact Information:</b>
+      - <b>Mobile:</b> +91 8299500552
+      - <b>Email:</b> <a href="mailto:Surendrayadav7668@gmail.com">Surendrayadav7668@gmail.com</a>
+      - <b>LinkedIn:</b> <a href="https://www.linkedin.com/in/surendra-y-04290316a" target="_blank">Surendra Yadav</a>
+      - <b>GitHub:</b> <a href="https://github.com/SUre-8299" target="_blank">SUre-8299</a>
+
+      I am eager to embrace new challenges and opportunities for growth in the IT field.`
+    ],
+    typeSpeed: 7,
+    contentType: 'html'
+  });
+}, 100);
